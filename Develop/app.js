84587a69816e1cf2bff0ae532b85c46c5ad3df3a
Sumnima B <sumnimabhandari@gmail.com>
2020-06-09 16:36:03 -0700

    Added some styling background colors in html files

--- Develop/app.js
+++ Develop/app.js
@@ -10,12 +10,9 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+// Empty array to hold objects created via user input
 const employees = [];
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-// ------------------------
 const ManagerQuestions = [
       { 
         type: "input",
@@ -115,7 +112,7 @@ const InternQuestions = [
   },
 ];
 
-// This function calls ManagerQuestions to begin with
+// This function calls ManagerQuestions to begin with. render function called and write the html output to team.html file under output folder.
 function init() {
  inquirer.prompt(ManagerQuestions)
  .then(data => {
@@ -141,7 +138,7 @@ function init() {
  });
 }
 
-// This function gets called if selected team is equal to "Engineer"
+// This function gets called if selected team is equal to "Engineer". render function called and write the html output to team.html file under output folder.
 function engineer() {
   inquirer.prompt(EngineerQuestions)
   .then(data => {
@@ -167,7 +164,7 @@ function engineer() {
   });
 }
 
-// This function gets called if selected team is equal to "Intern"
+// This function gets called if selected team is equal to "Intern". render function called and write the html output to team.html file under output folder.
 function intern() {
   inquirer.prompt(InternQuestions)
   .then(data => {
@@ -194,52 +191,3 @@ function intern() {
 }
 
 init();
-
-// -------------------------
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// function writeToFile(fileName, data) {
-
-//   const teamDetail = generateMarkdown(data);
-//   fs.writeFile(fileName, teamDetail, err => {
-//      if (err) {
-//        throw err;
-//      }
-//      console.log("Success!");
-//  });
-// }
-
-// function init() {
-//  inquirer.prompt(questions)
-//  .then(data => {
-//      writeToFile("./utils/README.md", data);
-//  })
-//  .catch(error => {
-//      throw error; 
-//  });
-// }
-
-
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
